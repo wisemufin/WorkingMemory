01431a382c4e1ed9de80c8fe07d108f40233fc95
Matthew Trabold <mct4240@gmail.com>
2019-04-30 21:01:39 -0500

    Updates

---
+++ precache-manifest.8cb214742d32600a110232e16e77dab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccda67eeb68c9e2c2f77c5934d0bc29",
+    "url": "/WorkingMemory/index.html"
+  },
+  {
+    "revision": "ce48831578346f660962",
+    "url": "/WorkingMemory/static/css/main.347fef39.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/WorkingMemory/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ce48831578346f660962",
+    "url": "/WorkingMemory/static/js/main.ca0e1b61.chunk.js"
+  },
+  {
+    "revision": "1695b218a0e3fc6117a0",
+    "url": "/WorkingMemory/static/js/runtime~main.686b8348.js"
+  }
+]);
